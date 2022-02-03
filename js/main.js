95f35ed3dda6674e877240722d49b32da1ecd757
adammak91 <adam210125@o2.pl>
2022-02-03 11:36:40 +0100

    zadanie28

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let name;
+name='dowolna';
+console.log('dowolna');
+
+let dowolna;
+dowolna='12';
+console.log('12');
+
+const nr = prompt(12);
+if(12>10){
+  console.log('podana liczba jest wieksza od 10');
+}
+else{
+  console.log("falsch");
+}
